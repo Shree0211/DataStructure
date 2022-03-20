4bd02cc368333bd817c30694acb1e1e160f71068
shree0211 <shreevyas0211@gmail.com>
2022-03-20 19:33:15 +0530

    Deleting Last Node

--- Data_Structure/Program.cs
+++ Data_Structure/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System.Collections.Generic;
-using Data_Structure; 
+using Data_Structure;
 
 LinkedList customLinkedList = new LinkedList();
 customLinkedList.AddLastNode(56);
@@ -7,6 +7,6 @@ customLinkedList.AddLastNode(30);
 customLinkedList.AddLastNode(70);
 customLinkedList.DisplayLinkedList();
 Console.WriteLine("-------------------------");
-int node = customLinkedList.DeleteFirstNode();
+int node = customLinkedList.DeleteLastNode();
 Console.WriteLine("{0} node deleted", node);
 customLinkedList.DisplayLinkedList();
