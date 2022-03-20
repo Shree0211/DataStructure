b9f127eeb1a3eb458125f404527b396b5cb00aa0
shree0211 <shreevyas0211@gmail.com>
2022-03-20 22:35:56 +0530

    Sorting Linked List

--- Data_Structure/Node.cs
+++ Data_Structure/Node.cs
@@ -6,13 +6,27 @@ using System.Threading.Tasks;
 
 namespace Data_Structure
 {
-        internal class Node
+    internal class Node
+    {
+        //Instance var.
+        public int data;
+        public Node next;
+        //Constructor to create a Node with data and next pointer.
+        public Node(int data)
         {
-            //Instance var.
-            public int data;
-            public Node next;
-            //Constructor to create a Node with data and next pointer.
-            public Node(int data)
+            this.data = data;
+            this.next = null;
+        }
+    }
+        public class SortedNode<T>
+        {
+            public T data;
+            public SortedNode<T> next;
+            /// <summary>
+            /// Initializes a new instance of the <see cref="SortedNode{T}"/> class.
+            /// </summary>
+            /// <param name="data">The data.</param>
+            public SortedNode(T data)
             {
                 this.data = data;
                 this.next = null;
--- Data_Structure/Program.cs
+++ Data_Structure/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Data_Structure;
+using static Data_Structure.Node;
 
 LinkedList customLinkedList = new LinkedList();
 customLinkedList.AddLastNode(56);
@@ -12,3 +13,15 @@ int key = customLinkedList.SearchNode(40);
 customLinkedList.DeleteAtParticularPosition(key);
 customLinkedList.DisplayLinkedList();
 customLinkedList.Size();
+
+SortedLinkedList<int> sortedLinkedList = new SortedLinkedList<int>();
+Console.WriteLine("<----SortedLinkedList---->");
+sortedLinkedList.Add(new SortedNode<int>(70));
+sortedLinkedList.Add(new SortedNode<int>(40));
+sortedLinkedList.Add(new SortedNode<int>(30));
+sortedLinkedList.Add(new SortedNode<int>(56));
+Console.WriteLine("LinkedList before sorting");
+sortedLinkedList.Display();
+sortedLinkedList.SortedList();
+Console.WriteLine("LinkedList after sorting");
+sortedLinkedList.Display();
